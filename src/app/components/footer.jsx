49e99a0867ee4611714b9cbc6b7736c7f5804ed3
Bayu Kurniawan <bayu05061992@gmail.com>
2025-05-16 09:41:36 +0700

    add footer

---
+++ src/app/components/footer.jsx
@@ -0,0 +1,12 @@
+// components/Footer.tsx
+export default function Footer() {
+  return (
+    <footer className="bg-purple-600 text-white py-4 mt-auto">
+      <div className="container mx-auto text-center">
+        <p>
+          Copyright © 2025 ITNERS CILANDAK.
+        </p>
+      </div>
+    </footer>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import Footer from "./components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Footer/>
       </body>
     </html>
   );
