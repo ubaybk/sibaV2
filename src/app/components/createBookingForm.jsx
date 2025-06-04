907ae81237419e74dde21e13ab5914b605089b2c
Bayu Kurniawan <bayu05061992@gmail.com>
2025-06-04 09:35:25 +0700

    mobile textBlack

--- src/app/components/createBookingForm.jsx
+++ src/app/components/createBookingForm.jsx
@@ -244,7 +244,7 @@ export default function CreateBookingForm() {
                     name="participantCount"
                     value={formData.participantCount}
                     onChange={handleChange}
-                    className="w-full px-4 py-3 bg-gray-50 border border-gray-300 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-purple-500 transition-all duration-200  md:text-gray-700"
+                    className="w-full px-4 py-3 bg-gray-50 border border-gray-300 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-purple-500 transition-all duration-200  sm:text-black"
                     placeholder="Enter number of participants"
                     required
                   />
@@ -260,7 +260,7 @@ export default function CreateBookingForm() {
                     name="startDate"
                     value={formData.startDate}
                     onChange={handleChange}
-                    className="w-full px-4 py-3 bg-gray-50 border border-gray-300 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-purple-500 transition-all duration-200"
+                    className="w-full px-4 py-3 bg-gray-50 border border-gray-300 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-purple-500 transition-all duration-200 md:text-black"
                     required
                   />
                 </div>
